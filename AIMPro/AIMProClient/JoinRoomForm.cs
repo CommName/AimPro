8505cc157e53fa6b3981319faac8427a3b5c346e
MarkoKrstic16153 <marko.krsticc@elfak.rs>
2019-12-20 14:57:46 +0100

    Dodate CreateRoom i JoinRoom

---
+++ AIMPro/AIMProClient/JoinRoomForm.cs
@@ -0,0 +1,31 @@
+﻿using AIMProClient.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIMProClient
+{
+    public partial class JoinRoomForm : Form
+    {
+        MenuController controller;
+        public JoinRoomForm(MenuController controller)
+        {
+            InitializeComponent();
+            this.controller = controller;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 4) {
+                MessageBox.Show("Oce da joinuje " + e.RowIndex + " sobu.");
+            }
+            
+        }
+    }
+}
